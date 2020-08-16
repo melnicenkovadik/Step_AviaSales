74417f0204017ad4fece649636eb9de9bd25abf7
Вадик Мельниченко <melnicenkovadik@gmail.com>
2020-08-16 18:05:47 +0300

    fetching problems

--- src/test/TestServiceF.java
+++ src/test/TestServiceF.java
@@ -1,6 +1,25 @@
 package test;
 
 
-public class TestServiceF {
+import entity.Flight;
+import junit.framework.TestResult;
+import org.junit.Test;
+import service.FlightService;
 
+import javax.lang.model.type.NullType;
+import java.lang.reflect.Array;
+import java.util.List;
+
+public class TestServiceF {
+    private static java.lang.Object Object;
+
+    public static void main(String[] args) {
+        FlightService flightService = new FlightService();
+        List<Flight> result = flightService.getFlights();
+        if (result != Object) {
+            System.out.println("Boroda");
+
+        }
+
+    }
 }

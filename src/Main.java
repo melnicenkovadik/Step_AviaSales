3fd99513e196a7bb1c2ccac96ba8a42ca0a5a2f1
VaDOsDanIT <62440835+VaDOsDanIT@users.noreply.github.com>
2020-08-23 05:24:50 +0300

    Pull (#19)
    * rebuild menu
    
    * Change menu
    
    Co-authored-by: ProskurinVadim <proskurin.vadim@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,47 @@
 import dao.AbstractDao;
 import dao.BookingDao;
-
-import dao.FlightDao;
-import entity.Booking;
-import entity.Flight;
-
-import enums.Destinations;
 import menu.Menu;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static boolean exit = false;
+    private static Scanner scanner = new Scanner(System.in);
+    private static AbstractDao dao = new BookingDao();
+    private static Menu menu = new Menu();
+
     public static void main(String[] args) {
-//        Menu menu = new Menu();
-//        menu.run();
-
-        AbstractDao dao = new FlightDao();
-
-        for (Destinations value : Destinations.values()) {
-            System.out.println(value.toString());
+        dao.loadDataFromFile();
+        while (!exit) {
+            menu.showMenu();
+            String answer = scanner.nextLine();
+            switch (answer) {
+                case "1":
+                    System.out.println("Scoreboard");
+                    if(exit()) break;
+                case "2":
+                    System.out.println("Flight info");
+                    if(exit()) break;
+                case "3":
+                    System.out.println("Search and booking");
+                    if(exit()) break;
+                case "4":
+                    System.out.println("Cancel booking");
+                    System.out.println(dao.getAll());
+                    if(exit()) break;
+                case "5":
+                    System.out.println("My flights");
+                    System.out.println(dao.getAll());
+                    if(exit()) break;
+                case "6":
+                    exit = true;
+                    break;
+            }
         }
-
-        System.out.println(dao.getAll());
+    }
+    private static boolean exit() {
+        System.out.println("Press 0 to return to menu");
+        return scanner.nextLine().equals("0");
     }
 }
--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,40 +1,21 @@
 package menu;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Menu {
-    private static boolean exit = false;
-    private static String answer = "";
-    private static Scanner scanner = new Scanner(System.in);
     enum MenuList{
-        FIRST("Show scoreboard"),
-        SECOND("show flight info"),
-        THIRD("Search and booking"),
-        FOURTH("Cancel booking"),
-        FIFTH("My flights"),
-        SIXTH("Exit");
+        FIRST("1. - Show scoreboard"),
+        SECOND("2. - Show flight info"),
+        THIRD("3. - Search and booking"),
+        FOURTH("4. - Cancel booking"),
+        FIFTH("5. - My flights"),
+        SIXTH("6. - Exit");
         String description;
         MenuList(String description){
             this.description = description;
         }
     }
 
-    public void run() {
-        while (!exit) {
-            for (MenuList menuItem : MenuList.values()){
-                System.out.println(menuItem +" " +menuItem.description);
-            }
-            System.out.println("Choose menu item");
-            answer = scanner.nextLine();
-            switch (answer) {
-                case "FIRST" :
-                    break;
-                case "SECOND" :
-                    break;
-                case "SIX" :
-                    exit = true;
-            }
+    public void showMenu() {
+        for (MenuList menuItem : MenuList.values()){
+            System.out.println(menuItem.description);
         }
     }
 }
